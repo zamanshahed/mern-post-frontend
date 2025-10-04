ece5fffbbf32b88a79b73b0b03ae5c398d6bccab
zamanshahed <sumonshd@gmail.com>
2025-10-05 00:02:48 +0600

    initial commit...

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="text-emerald-400 text-4xl font-thin flex items-center justify-center h-screen">
+      Hey Mom! I am a full stack developer now!
+    </div>
+  );
+}
+
+export default App;
